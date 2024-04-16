209df5cdbf3a34182abc665a6e761edf9519c720
kotoju <katoju.akhil@centific.com>
2024-04-16 17:32:35 +0530

    H1 Change

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Akhil</h1>
+      <h1>Kotoju Akhil</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
